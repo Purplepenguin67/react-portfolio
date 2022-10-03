6c015580e1a4b199606bbca4b33febf440bb879b
Douglas Snodgrass <103548864+Purplepenguin67@users.noreply.github.com>
2022-10-03 01:38:43 -0700

    got it working

--- src/resumeData.js
+++ src/resumeData.js
@@ -2,29 +2,29 @@ let resumeData = {
     "imagebaseurl":"https://rbhatia46.github.io/",
     "name": "Douglas Snodgrass",
     "role": "Dull Stack Developer",
-    "linkedinId":"Your LinkedIn Id",
+    "linkedinId": "https://www.linkedin.com/in/douglas-snodgrass-6580233a/",
     "skypeid": "Your skypeid",
     "roleDescription": "I like dabbling in various parts of full stack development and like to learn about new technologies, write technical articles or simply play games in my free time.",
     "socialLinks":[
         {
           "name":"linkedin",
-          "url":"https://www.linkedin.com/in/douglas-snodgrass-6580233a/",
+          "url": "https://www.linkedin.com/in/douglas-snodgrass-6580233a/",
           "className":"fa fa-linkedin"
         },
         {
           "name":"github",
-          "url":"http://github.com/Purplepenguin67",
+          "url": "http://github.com/Purplepenguin67",
           "className":"fa fa-github"
         },
         {
           "name":"skype",
-          "url":"http://twitter.com/snodgrass6879",
+          "url": "http://twitter.com/snodgrass6879",
           "className":"fa fa-twitter"
         }
       ],
-    "aboutme":"I started my coding journey almost 6 months ago and now I'm working full time and doing freelance projects on the side for my friends. You may have noticed from my portfolio, but I'm obsessed with unique, interesting design and user-friendly functionality so hit me up with your weird artistic project ideas or app proposals.I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-    "address":"India",
-    "website":"http://github.com/Purplepenguin67",
+    "aboutme":"I started my coding journey almost 6 months ago and now I'm working full time and doing freelance projects on the side for my friends. You may have noticed from my portfolio, but I'm obsessed with unique, interesting design and user-friendly functionality so hit me up with your weird artistic project ideas or app proposals. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"Los Angeles, CA",
+    "website": "http://github.com/Purplepenguin67",
     "education":[
       {
         "UniversityName":"University of California Los Angeles",
@@ -59,17 +59,17 @@ let resumeData = {
       {
         "name":"Shaken and Stirred",
         "description":"cocktail recipie app",
-        "imgurl":"images/portfolio/phone.jpg"
+        "imgurl":"public/images/portfolio/alcohol.jpg"
       },
       {
         "name":"total fitness plus",
         "description":"workout journal",
-        "imgurl":"images/portfolio/project.jpg"
+        "imgurl":"public/images/portfolio/fitness.jpg"
       },
       {
         "name":"weather tracker",
-        "description":"mobileapp",  
-        "imgurl":"images/portfolio/project2.png"
+        "description":"an app to check weather",  
+        "imgurl":"public/images/portfolio/weather.jpg"
       }
     ]
     
